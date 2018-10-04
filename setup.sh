547da2de6b95c464093ae768cc8adcb352befbf0
Muhammad Zumair <zumairmuhammad@gmail.com>
2018-10-04 18:58:20 +0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#install curl
+sudo apt install curl
+
+#install node and npm LTS version
+curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
+sudo apt install nodejs
+
+#install firebase
+sudo npm install firebase-tools -g
+
+#install typescript
+sudo npm install -g typescript
+
+#install git
+sudo apt install git
